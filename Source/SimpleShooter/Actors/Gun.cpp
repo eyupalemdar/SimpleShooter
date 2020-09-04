27864678a88de40becfb3d867620a3103a8bd1de
eyupalemdar <eyupalemdar@outlook.com>
2020-09-04 17:26:15 +0300

    Gun Actors

---
+++ Source/SimpleShooter/Actors/Gun.cpp
@@ -0,0 +1,26 @@
+#include "Gun.h"
+#include "Components/SkeletalMeshComponent.h"
+
+AGun::AGun()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh");
+	Mesh->SetupAttachment(Root);
+}
+
+void AGun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/SimpleShooter/Actors/Gun.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Gun.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API AGun : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AGun();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* Root = nullptr; // Parent class already has RootComponent so we name it as Root
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* Mesh = nullptr;
+
+};
